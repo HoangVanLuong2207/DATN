3d6f1810f01f5b32fe06881b03e18d995bccf977
chiennm <chiennmpp03120@gmail.com>
2025-05-09 16:43:48 +0700

    Hoàn thành danhmuc admin

--- DATN/routes/web.php
+++ DATN/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\admin\DanhmucController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[Controller::class,'index']);
+// Danh mục
+Route::get('/admin',[HomeController::class, 'index'])->name('home.index');
+Route::get('/admin/danhmuc',[DanhmucController::class,'index'])->name('danhmuc.index');
+Route::get('/admin/danhmuc/create',[DanhmucController::class,'create'])->name('danhmuc.create');
+Route::post('/admin/danhmuc/store',[DanhmucController::class,'store'])->name('danhmuc.store');
+Route::get('/admin/danhmuc/delete/{id}',[DanhmucController::class,'delete'])->name('danhmuc.delete');
+Route::get('/admin/danhmuc/edit/{id}',[DanhmucController::class,'edit'])->name('danhmuc.edit');
+Route::post('/admin/danhmuc/update/{id}',[DanhmucController::class,'update'])->name('danhmuc.update');
